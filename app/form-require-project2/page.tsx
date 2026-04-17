621a4e6b5719f75b3c9a27b51fe0522c330e00bc
dhanywp20-cmyk <dhanywp20@gmail.com>
2026-04-18 00:47:00 +0700

    Update page.tsx

--- app/form-require-project2/page.tsx
+++ app/form-require-project2/page.tsx
@@ -1912,7 +1912,7 @@ function FormRequireProject({ currentUser }: { currentUser: User }) {
                               </>
                             )}
                             <button onClick={() => handleOpenDetail(req)} title="Lihat Detail"
-                              className="text-teal-600 hover:text-teal-800 transition-colors" title="View Detail">
+                              className="text-red-400 hover:text-red-600 transition-colors">
                               <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>
                             </button>
                             {(isSuperAdmin || isAdmin) && (
